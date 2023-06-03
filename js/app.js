160968458bd329d881c44ea2f2fe89be61e03c39
Ajamu Page <kil91402@gmail.com>
2023-06-03 12:04:36 -0500

    added array

---
+++ js/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let votingArea = document.getElementById("voting-area");
+let resultsArea = document.getElementById("voting-area");
+
+function Goat(name, imgSrc) {
+  this.name = name;
+  this.imgSrc = imgSrc;
+  this.voteCount = 0;
+  this.viewCount = 0;
+}
+
+let cruisinGoat = new Goat("CrusinGoat", "./img/crusin-goat.jpg");
+let floatGoat = new Goat("FloatYourGoat", "./img/float-your-goat.jpg");
+
+let goatArray = [];
+
+goatArray.push(cruisinGoat);
+goatArray.push(floatGoat);
+
+console.log(goatArray);
